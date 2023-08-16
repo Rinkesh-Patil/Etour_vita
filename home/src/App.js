887900a7b9b6ad413d0216f74c6b81da6207f8c6
Sarang V Sankhe <sarangvsankhe25@gmail.com>
2023-08-16 16:32:46 +0530

    Card Spacing

--- home/src/App.js
+++ home/src/App.js
@@ -1,3 +1,4 @@
+import { alignPropType } from 'react-bootstrap/esm/types';
 import './App.css';
 import Card from './components/card';
 import Footer from './components/footer';
@@ -5,17 +6,22 @@ import Footer from './components/footer';
 import Header from './components/navbar';
 function App() {
   return (
+    
     <div>
       <Header/>
-      <div className='fcontainer'>
+      
+      <div  className='first-half'>
       <Card/>
       <Card/>
       <Card/>
+    </div>
+    <h1 style={{textAlign:'center'}}>All inclusive tours </h1>
+    <div className='sec-half'>
       <Card/>
       <Card/>
       <Card/>
       </div>
-      <Footer/>
+      {/* <Footer/> */}
     </div>
   );
 }
